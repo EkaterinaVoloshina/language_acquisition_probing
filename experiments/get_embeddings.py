3e788fa7acfd24f0e60ebb5f618c4a2908bee5eb
Ekaterina Voloshina <42929264+EkaterinaVoloshina@users.noreply.github.com>
2021-12-06 10:20:31 +0300

    Update get_embeddings.py

--- experiments/get_embeddings.py
+++ experiments/get_embeddings.py
@@ -52,7 +52,7 @@ class Embeddings(object):
         return tensor_dataloader
 
     def mean_pooling(self, model_output, attention_mask):
-        tokens = np.zeros((32, 512, 7))
+        tokens = np.zeros((self.batch_size, self.size, 7))
         for num, i in enumerate(model_output):
             tokens[:,:, num] = i[:,0,:].cpu().detach().numpy()
         input_mask_expanded = attention_mask.unsqueeze(-1).expand(tokens.shape).float().cpu().detach().numpy()
